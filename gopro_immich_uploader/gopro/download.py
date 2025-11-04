5075620340545d5adf57acdcd8fa2bbbcc800d29
Jiří Kuchyňka (Anty) <anty150@gmail.com>
2025-11-04 11:04:08 +0100

    fix: try a whole barrage of options to get the camera to delete a file

--- gopro_immich_uploader/gopro/download.py
+++ gopro_immich_uploader/gopro/download.py
@@ -13,20 +13,71 @@ from gopro_immich_uploader.progress_reporting_iterator import ProgressReportingI
 log = get_logger(__name__)
 
 
+async def try_delete_file(camera: WirelessGoPro, file: MediaItem) -> bool:
+    try:
+        response = await camera.http_command.delete_file(path=file.filename)
+        if response.ok:
+            return True
+        log.warning("Delete for %s failed", file.filename)
+    except Exception as e:
+        log.exception(f"Delete for %s threw exception: %s", file.filename, e, exc_info=e)
+    return False
+
+async def check_media_list(camera: WirelessGoPro, file: MediaItem):
+    files = (await camera.http_command.get_media_list()).data.files
+    found_file = file.filename in [f.filename for f in files]
+    if not found_file:
+        log.warning("File %s no longer found in media list", file.filename)
+    else:
+        log.warning("File %s still found in media list", file.filename)
+
+
 async def delete_file(camera: WirelessGoPro, file: MediaItem):
-    # Retry delete up to 10 times (GoPro delete endpoint is unreliable)
-    max_retries = 10
-    for attempt in range(1, max_retries + 1):
-        try:
-            # Give time for the camera to realize the file is no longer in use,
-            # no idea if it really helps
-            await asyncio.sleep(1)
-            response = await camera.http_command.delete_file(path=file.filename)
-            if response.ok:
-                break
-        except Exception as e:
-            log.exception(f"Delete attempt %d/%d for %s threw exception: %s", attempt, max_retries, file.filename, e, exc_info=e)
-        log.warning("Delete attempt %d/%d for %s failed", attempt, max_retries, file.filename)
+    # Give time for the camera to realize the file is no longer in use,
+    # no idea if it really helps
+    await asyncio.sleep(1)
+    if await try_delete_file(camera, file):
+        return
+
+    # We probably got a 400: Requested delete path was not found in the media list.
+    # This should not happen, yet it happens regularly...
+    # Let's try to fetch the media list again, maybe that will fix it
+    await asyncio.sleep(1)
+    log.warning("Retry delete for %s: Fetch media list before delete", file.filename)
+    await check_media_list(camera, file)
+    if await try_delete_file(camera, file):
+        return
+
+    try:
+        # Still nothing? Fuggin GoPro...
+        # Try one more time with turbo mode disabled
+        log.warning("Retry delete for %s: Without turbo mode", file.filename)
+        await camera.http_command.set_turbo_mode(mode=Toggle.DISABLE)
+        await asyncio.sleep(1)
+        if await try_delete_file(camera, file):
+            return
+    finally:
+        await camera.http_command.set_turbo_mode(mode=Toggle.ENABLE)
+
+    # Last try before we give up
+    # Maybe rebooting the camera will help?
+    log.warning("Retry delete for %s: Rebooting camera", file.filename)
+    try:
+        await camera.http_command.reboot()
+        await asyncio.sleep(30)  # Give the camera time to reboot and reconnect (I hope this works)
+        if await try_delete_file(camera, file):
+            return
+
+        # Maybe media again?
+        log.warning("Retry delete for %s: Rebooting camera: Fetch media list before delete", file.filename)
+        await check_media_list(camera, file)
+        if await try_delete_file(camera, file):
+            return
+    finally:
+        # Re-enable turbo mode after rebooting
+        await camera.http_command.set_turbo_mode(mode=Toggle.ENABLE)
+
+    log.error("Delete for %s failed: I give up, delete the file manually", file.filename)
 
 
 async def download_files(
